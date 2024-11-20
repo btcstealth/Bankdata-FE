693a9c103bd01cf36374ca666b817ef32dab082c
unknown <bjarketc@protonmail.com>
2024-11-20 17:22:12 +0100

    added currency-exchange component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyExchangeComponent } from './currency-exchange/currency-exchange.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyExchangeComponent
   ],
   imports: [
     BrowserModule,
