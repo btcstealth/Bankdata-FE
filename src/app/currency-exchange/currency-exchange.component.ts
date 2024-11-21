c957d87b15dce4f6a961a1cb53d335828ffa53fc
unknown <bjarketc@protonmail.com>
2024-11-21 09:01:22 +0100

    added last missing work

--- src/app/currency-exchange/currency-exchange.component.ts
+++ src/app/currency-exchange/currency-exchange.component.ts
@@ -2,10 +2,28 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export enum CurrencyUnitEnum {
-  USD,
-  DKK,
-  EUR
+export interface CurrencyExchangeData {
+  currencyType: string;
+  amount: string;
+
+  /*
+  constructor(data: { currencyType: string; amount: string; }) {
+    this.currencyType = data.currencyType;
+    this.amount = data.amount;
+  }
+
+  public getCurrencyType(): string {
+    return `${this.currencyType}px`;
+  }
+
+  public getAmount(): string {
+    return `${this.amount}px`;
+  }
+
+  public toString(): string {
+    return this.getCurrencyType() + ' : ' + this.getAmount();
+  }
+    */
 }
 
 @Component({
@@ -22,7 +40,10 @@ export class CurrencyExchangeComponent {
 
   fromCurrency: string = 'DKK';
   toCurrency: string = 'USD';
-  currencyExchangeResponse: number = 0.0;
+  fundsToExchange: number = 0.0;
+  currencyExchangeRate: number = 0.0;
+  //create json object
+  currencyExchangeAmount: CurrencyExchangeData[] = [];
 
   displayRequestError: boolean = false;
 
@@ -30,30 +51,45 @@ export class CurrencyExchangeComponent {
   }
 
   ngOnInit() {
-    this.addEventListener();
+    this.addEventListeners();
   }
 
-  private addEventListener(): void {
-    const button = document.getElementById('currencyExchangeBtn') as HTMLButtonElement;
-      button.addEventListener('click', () => {
-      this.getExchangeRateData()
-        	.subscribe({
-            next: (response: number) => {
-              this.displayRequestError = false;
-              this.currencyExchangeResponse = response
-            },
-            error: (error) => {
-              this.displayRequestError = true;
-              console.error(`Error occurred: ${error.message}`)
-            },
-            complete: () => console.log('Observable completed')
-          });
+  private addEventListeners(): void {
+    const currencyExchangeBtn = document.getElementById('currencyExchangeBtn') as HTMLButtonElement;
+    currencyExchangeBtn.addEventListener('click', () => {
+    this.getExchangeRateData().subscribe({
+        next: (exchangeRate: number) => {
+          this.displayRequestError = false;
+          this.currencyExchangeRate = exchangeRate
+        },
+        error: (error) => {
+          this.displayRequestError = true;
+          console.error(`Error : ${error.message}`)
+        },
+        complete: () => console.log('Completed') });
     });
+
+
+
+    const currencyExchangeAmountBtn = document.getElementById('currencyExchangeAmountBtn') as HTMLButtonElement;
+    currencyExchangeAmountBtn.addEventListener('click', () => {
+      this.getExchangeData().subscribe({
+          next: (response: string) => {
+            this.displayRequestError = false;
+            //const test = JSON.parse(response);
+          },
+          error: (error) => {
+            this.displayRequestError = true;
+            console.error(`Error : ${error.message}`)
+          },
+          complete: () => console.log('Completed') });
+      });
+
   }
 
   //TODO: this should ideally be placed in currency-exchange.service.ts.
   //But I'm missing something obvious in order to get dependency injection for CurrencyExchangeService working in the constructor above.
-  private getExchangeRateData(): Observable<number> {
+  private getExchangeRateData(): Observable<any> {
     const localhostUrl: string = 'http://localhost:8080';
     const endpointUrl: string = '/exchange/rate/';
 
@@ -61,6 +97,12 @@ export class CurrencyExchangeComponent {
     //params.set('fromCurrency', this.fromCurrency);
     //params.set('toCurrency', this.toCurrency);
     return this.http.get<number>(localhostUrl + endpointUrl + this.fromCurrency + '/' + this.toCurrency);
+  }
+
+  private getExchangeData(): Observable<string> {
+    const localhostUrl: string = 'http://localhost:8080';
+    const endpointUrl: string = '/exchange/';
+    return this.http.get<string>(localhostUrl + endpointUrl + this.fromCurrency + '/' + this.toCurrency + '/' + this.fundsToExchange);
   }
 
 }
